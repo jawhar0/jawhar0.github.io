675fcecde5492126228f1143290299f91900d9ea
maherjaafar <maher.jaafar@hotmail.com>
2020-11-14 10:05:18 +0100

    deleted youtube part

--- lib/main.dart
+++ lib/main.dart
@@ -255,10 +255,10 @@ class _MyHomePageState extends State<MyHomePage> {
                 title: "Public github repos",
               ),
               MyGitHubReposWidget(),
-              TitleWidget(
-                title: "Youtube videos",
-              ),
-              YoutubeVideosWidget(),
+              // TitleWidget(
+              //   title: "Youtube videos",
+              // ),
+              // YoutubeVideosWidget(),
             ],
           ),
         ),
